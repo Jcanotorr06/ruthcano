e6420f21272779c7ae94dc98549ec04651baeab2
Jcanotorr06 <jcano062001@gmail.com>
2023-01-04 20:51:01 -0500

    update: async contact form

--- pages/api/contact.ts
+++ pages/api/contact.ts
@@ -14,22 +14,42 @@ const handler:NextApiHandler = async (req, res) => {
         },
     })
 
+    // Verify connection configuration
+    await new Promise((resolve, reject) => {
+        transporter.verify((error, success) => {
+            if (error) {
+                console.log(error)
+                reject(error)
+                res.status(500).json({ error: "Server error" })
+            } else {
+                console.log("Server is ready to take our messages")
+                resolve(success)
+                return true
+            }
+        })
+    })
+
     const mailData = {
         from: "\"Portfolio Contact Form\" <llinus907@gmail.com>",
-        to: process.env.EMAIL_CONTACT_TO,
+        to: "josephct06@gmail.com",
         subject: `Message from ${req.body.name} with email ${req.body.email} from your website`,
         text: req.body.message,
         html: emailTemplate(req.body.name, req.body.email, req.body.message),
     }
 
-    transporter.sendMail(mailData, (error, info) => {
-        if (error) {
-            console.log(error)
-            res.status(500).json({ message: "Something went wrong" })
-        } else {
-            console.log(info)
-            res.status(200).json({ message: "Message sent successfully" })
-        }
+    await new Promise((resolve, reject) => {
+        // send mail with defined transport object
+        transporter.sendMail(mailData, (error, info) => {
+            if (error) {
+                console.log(error)
+                reject(error)
+                res.status(500).json({ error: "Server error" })
+            } else {
+                console.log("Message sent: %s", info.messageId)
+                resolve(info)
+                res.status(200).json({ success: "Message sent" })
+            }
+        })
     })
 }
 
