b401312670c6aa18645ec4582bf373f8ae6b2960
Jcanotorr06 <jcano062001@gmail.com>
2023-01-04 19:05:28 -0500

    update: contact page

--- components/Landing/CTA.tsx
+++ components/Landing/CTA.tsx
@@ -1,13 +1,27 @@
 import { type FC } from "react"
+import { useRouter } from 'next/router';
 
 const CTA:FC = () => {
+
+  const router = useRouter()
+
+  const handleContactMe = () => {
+    router.push("/contact")
+  }
+
   return (
     <section className="flex flex-col justify-center items py-24 gap-[10px] w-full">
       <article className="flex py-11 gap-[10px]">
-        <div className="w-full">
-          <h2 className="ui-3xl lg:ui-7xl xl:ui-8xl upper font-semibold text-center text-surface">
-            Dui quis viverra nec tempus mollis.
+        <div className="w-full text-center flex flex-col gap-4">
+          <h2 className="ui-xl lg:ui-3xl xl:ui-4xl upper font-semibold text-center justify-center items-center text-surface">
+            If you have a general or project inquiry, please feel free to contact me.
           </h2>
+          <button 
+            onClick={handleContactMe}
+            className="ui-sm self-center lg:ui-xl upper w-fit font-semibold text-center btn hover:underline rounded-full px-12"
+          >
+            Contact Me
+          </button>
         </div>
       </article>
     </section>
