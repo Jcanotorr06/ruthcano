79d38e112821008430cf40d1000a974b9fd08684
Jcanotorr06 <jcano062001@gmail.com>
2023-03-20 17:46:29 -0500

    update: CTA.tsx. change CTA message

--- components/Landing/CTA.tsx
+++ components/Landing/CTA.tsx
@@ -1,25 +1,23 @@
-import { type FC } from "react"
-import { useRouter } from 'next/router';
+import { type FC } from "react";
+import { useRouter } from "next/router";
 
-const CTA:FC = () => {
-
-  const router = useRouter()
+const CTA: FC = () => {
+  const router = useRouter();
 
   const handleContactMe = () => {
-    router.push("/contact")
-  }
+    router.push("/contact");
+  };
 
   return (
     <section className="flex flex-col justify-center items py-24 gap-[10px] w-full">
       <article className="flex py-11 gap-[10px]">
         <div className="w-full text-center flex flex-col gap-4">
           <h2 className="ui-xl lg:ui-3xl xl:ui-4xl upper font-semibold text-center justify-center items-center text-surface">
-          If you would like to know more about my work
-          or talk about a project
-          check out my social media links
-          or feel free to fill out the contact form.
+            I love what I do, and I am very good at it.
+            <br />
+            Do you want to work with me?
           </h2>
-          <button 
+          <button
             onClick={handleContactMe}
             className="ui-sm self-center lg:ui-xl upper w-fit font-semibold text-center btn hover:underline rounded-full px-12"
           >
@@ -28,7 +26,7 @@ const CTA:FC = () => {
         </div>
       </article>
     </section>
-  )
-}
+  );
+};
 
-export default CTA
+export default CTA;
